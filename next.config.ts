70f4e6446764c8d22204612a85ac690b1f2eba7f
Jirapat Promta <jirapat.p@arise.tech>
2025-02-16 07:34:01 +0700

    setup deploy github page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /**
+   * Enable static exports.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/mockup",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
